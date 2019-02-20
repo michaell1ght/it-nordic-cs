159e28d6f7264dfeda84c615f7b21d251b63c013
Michael <michail.golovkov@gmail.com>
2019-02-20 21:58:05 +0300

    ycommit

---
+++ Lesson1/ClassWork/DemoApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите имя");
+			string name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Hello {name}");
+			Console.ReadKey();
+		}
+	}
+}
