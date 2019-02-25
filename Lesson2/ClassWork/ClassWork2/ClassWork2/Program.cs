b2e9eaee97bd6e148fc6f8769be6bafe3feb0a11
Michael <michail.golovkov@gmail.com>
2019-02-25 21:43:48 +0300

    Lesson

---
+++ Lesson2/ClassWork/ClassWork2/ClassWork2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassWork2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		Console.WriteLine ("Введите радиус круга");
+		float CircleRadius= float.Parse(Console.ReadLine());
+		float result = (float) Math.Pow(CircleRadius, 2) *(float)Math.PI;
+		Console.WriteLine($"Площадь круга ={result}");
+		Console.ReadKey();
+		}
+	}
+}
