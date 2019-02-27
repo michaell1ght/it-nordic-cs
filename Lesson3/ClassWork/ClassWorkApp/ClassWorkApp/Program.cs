3c58e29df8db289abf2a28d20994f66e274e6496
Michael <michail.golovkov@gmail.com>
2019-02-27 21:33:42 +0300

    classWork3

---
+++ Lesson3/ClassWork/ClassWorkApp/ClassWorkApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassWorkApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] arr = new string[5];
+			int i = 0;
+			for (i = 0; i < arr.Length; i++)
+			{
+				Console.WriteLine($"input {i+1} value");
+				arr[i] = Console.ReadLine();
+			}
+			for (i = 0; i < arr.Length; i++)
+			{
+				Console.WriteLine($"value number {i+1} = {arr[i]}");
+			}
+				Console.ReadKey();
+		}
+	}
+}
