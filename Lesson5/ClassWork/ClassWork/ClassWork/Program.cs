85dde939dc68fdb0f496e6a1904f52f0d211cf9f
Michael <michail.golovkov@gmail.com>
2019-03-06 21:31:59 +0300

    ClassWork

---
+++ Lesson5/ClassWork/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 0;
+			Console.WriteLine("введите число");
+			try
+			{
+				i = int.Parse(Console.ReadLine());
+				Console.WriteLine(i < 0 ? "отрицательное" : "неотрицателньое");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"{e.GetType()} , {e.Message}");
+			}
+			Console.ReadLine();
+		}
+	}
+}
