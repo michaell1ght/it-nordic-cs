062a4d7f9c0b3667de1fb2d95a73b26d55e2b88c
Michael <michail.golovkov@gmail.com>
2019-03-11 21:25:35 +0300

    Classwork

---
+++ Lesson6/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var marks = new[]
+			{
+			new[] {2,3,3,2,3},
+			new[] {2,4,5,3},
+			null,
+			new[] { 5,5,5,5},
+			new[] { 4}
+			};
+			int i = 0;
+			int j = 0;
+			int sumByDay=0;
+			int sumByWeek=0;
+			for (i=0;i<marks[i].Length;i++)
+			{
+				sumByWeek += marks[i];
+				for (j = 0; j < marks[j].Length; j++)
+				{
+					sumByDay= marks[j];
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
+//arr1 от 1 до 5 - массив оценок
+//arr2 от 1 до 5
