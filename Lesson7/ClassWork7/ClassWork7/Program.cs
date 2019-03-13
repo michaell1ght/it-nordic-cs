275277e8975bde5b9fc183f445bc31bf9e81630f
Michael <michail.golovkov@gmail.com>
2019-03-13 21:14:46 +0300

    ClassWork7

---
+++ Lesson7/ClassWork7/ClassWork7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassWork7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a = 0;
+			double b = 0;
+			Console.WriteLine("введите дробное число 1");
+			a = double.Parse(Console.ReadLine());
+			Console.WriteLine("введите дробное число 2");
+			b = double.Parse(Console.ReadLine());
+			double c = a * b;
+			//2 знака после запятой
+			Console.WriteLine(a + " + " + b +" = " + (a + b));
+			Console.WriteLine("{0:#.##} + {1:#.##} = {2:#.##}", a , b, a + b);
+			Console.WriteLine($"{a:#.##} - {b:#.##} = {a - b:#.##}");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lesson7/StringMethods/StringMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringMethods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a = "my test string";
+			Console.WriteLine(a.Substring(8, 182));
+			Console.ReadKey();
+		}
+	}
+}
