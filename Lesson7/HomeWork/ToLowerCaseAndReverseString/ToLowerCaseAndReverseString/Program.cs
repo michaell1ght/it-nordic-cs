a240724a65306092fd6c8ba23a8336cdb9151ca3
L1ght <38190449+michaell1ght@users.noreply.github.com>
2019-03-18 12:39:36 +0300

    Update Program.cs

--- Lesson7/HomeWork/ToLowerCaseAndReverseString/ToLowerCaseAndReverseString/Program.cs
+++ Lesson7/HomeWork/ToLowerCaseAndReverseString/ToLowerCaseAndReverseString/Program.cs
@@ -1,36 +1,36 @@
-﻿using System;
+using System;
 
 namespace ToLowerCaseAndReverseString
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int i = 0;
             char[] reverseSymbolsArray;
             string inputString = null;
             string[] inputStringArray = {null};
-            const char STRINGTOARRAYSEPARATOR = ' ';
-            const int INPUTSTRINGLENGHT= 2;
+            const char stringToArraySeparator = ' ';
+            const int inputStringLength= 2;
             Console.WriteLine("Данное приложение приводит строку к нижнему регистру и записывает введённую строку в обратном порядке");
             Console.WriteLine("Введите строку");
             inputString = Console.ReadLine();
 
             while (String.IsNullOrWhiteSpace(inputString)
-                && IsUnPrintableLiteralsInStringCheck(inputString)
-                && inputString.ToCharArray().Length < INPUTSTRINGLENGHT == true)
+                || IsUnPrintableLiteralsInStringCheck(inputString)
+                || inputString.ToCharArray().Length < inputStringLength == true)
             {
                 Console.WriteLine("Ошибка! Нужно ввести 2 числа или 2 буквы. Попробуйте ещё раз");
                 inputString = Console.ReadLine();
             }
 
-            Array.Reverse(inputStringArray = inputString.ToLower().Split(STRINGTOARRAYSEPARATOR));
+            Array.Reverse(inputStringArray = inputString.ToLower().Split(stringToArraySeparator));
                 for (i = 0; i < inputStringArray.Length; i++)
                 {
                     reverseSymbolsArray = inputStringArray[i].ToCharArray();
                     Array.Reverse(reverseSymbolsArray);
                     string reverseString = new string(reverseSymbolsArray);
-                    Console.Write($"{reverseString} ");
+                    Console.Write("{1} ",reverseString);
                 }
                 Console.ReadKey();
         }
