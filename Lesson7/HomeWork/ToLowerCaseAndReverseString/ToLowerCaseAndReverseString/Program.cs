618e1785cbffc7056932d72eb6717c151c7471bb
Michail <michail.golovkov@gmail.com>
2019-03-18 04:41:50 +0300

    HomeWork

--- Lesson7/HomeWork/ToLowerCaseAndReverseString/ToLowerCaseAndReverseString/Program.cs
+++ Lesson7/HomeWork/ToLowerCaseAndReverseString/ToLowerCaseAndReverseString/Program.cs
@@ -10,28 +10,21 @@ namespace ToLowerCaseAndReverseString
             char[] reverseSymbolsArray;
             string inputString = null;
             string[] inputStringArray = {null};
-            const char SEPARATOR = ' ';
+            const char STRINGTOARRAYSEPARATOR = ' ';
             const int INPUTSTRINGLENGHT= 2;
             Console.WriteLine("Данное приложение приводит строку к нижнему регистру и записывает введённую строку в обратном порядке");
             Console.WriteLine("Введите строку");
             inputString = Console.ReadLine();
-            Console.WriteLine(String.IsNullOrWhiteSpace(inputString));
-            Console.WriteLine(IsUnPrintableLiteralsInStringCheck(inputString));
-            Console.WriteLine(inputString.Length > INPUTSTRINGLENGHT);
 
             while (String.IsNullOrWhiteSpace(inputString)
                 && IsUnPrintableLiteralsInStringCheck(inputString)
                 && inputString.ToCharArray().Length < INPUTSTRINGLENGHT == true)
             {
-                Console.WriteLine(String.IsNullOrWhiteSpace(inputString));
-                Console.WriteLine(IsUnPrintableLiteralsInStringCheck(inputString));
-                Console.WriteLine(inputString.ToCharArray().Length > INPUTSTRINGLENGHT);
-
                 Console.WriteLine("Ошибка! Нужно ввести 2 числа или 2 буквы. Попробуйте ещё раз");
                 inputString = Console.ReadLine();
             }
 
-            Array.Reverse(inputStringArray = inputString.ToLower().Split(SEPARATOR));
+            Array.Reverse(inputStringArray = inputString.ToLower().Split(STRINGTOARRAYSEPARATOR));
                 for (i = 0; i < inputStringArray.Length; i++)
                 {
                     reverseSymbolsArray = inputStringArray[i].ToCharArray();
