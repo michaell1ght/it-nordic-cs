b6d3899720570d0fcdb4fb6911998a1b84af7028
L1ght <38190449+michaell1ght@users.noreply.github.com>
2019-03-25 01:47:47 +0300

    Update Program.cs

--- Lesson7/HomeWork/WordsStartWithACount/WordsStartWithACount/Program.cs
+++ Lesson7/HomeWork/WordsStartWithACount/WordsStartWithACount/Program.cs
@@ -6,27 +6,32 @@ namespace WordsStartWithACount
     {
         static void Main(string[] args)
         {
+            const char stringSeparator = ' ';
+
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
             System.Threading.Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.InvariantCulture;
             const int inputWordsMinimalQuantity = 2;
+            
             int i = 0;
             int count = 0;
             string outputPhrase = "количество слов начинающихся с русской буквы буквы 'А'";
             string inputString = null;
             string[] inputStringArray = { null };
-            const char stringSeparator = ' ';
+            
             Console.WriteLine($"Данное приложение считает {outputPhrase} ");
             Console.WriteLine("введите строку");
             inputString = Console.ReadLine();
             inputStringArray = inputString.Split(stringSeparator);
+            
             while (String.IsNullOrWhiteSpace(inputString) == true || inputStringArray.Length < inputWordsMinimalQuantity)
             {
                 Console.WriteLine("Ошибка! Нужно ввести хотя бы 2 слова. Попробуйте ещё раз");
                 inputString = Console.ReadLine();
                 inputStringArray = inputString.Split(stringSeparator);
             }
+            
             foreach (string stringArrayElement in inputStringArray)
             {
                 if (stringArrayElement.StartsWith("а" ,true, System.Threading.Thread.CurrentThread.CurrentCulture))
@@ -34,9 +39,11 @@ namespace WordsStartWithACount
                     i++;
                 }
             }
+            
             Console.WriteLine($"{SetFirstStringLetterToUpperCase(outputPhrase)} = {i}");
             Console.ReadKey();
         }
+        
         public static string SetFirstStringLetterToUpperCase(string someString)
         {
             return someString = someString.Substring(0, 1).ToUpper() + someString.Substring(1, someString.Length - 1);
