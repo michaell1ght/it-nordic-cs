8e7831a38491dc04d2cec7da4d6ceb3719233100
Michael <michail.golovkov@gmail.com>
2019-03-25 21:20:00 +0300

    ClassWork10

---
+++ Lesson10/ClassWork/OOP/OOP/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+	public class Pet
+	{
+		public string Name;
+		public string Kind;
+		public char Sex;
+		public byte Age;
+
+		public string Description
+	{
+		get
+		{
+			return $"{Name} is a {Kind} ({Sex}) of {Age} years old";
+		}
+	}
+	}
+}
---
+++ Lesson10/ClassWork/OOP/OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet();
+			pet1.Name = "Котейка";
+			pet1.Kind = "Cat";
+			pet1.Sex = 'F';
+			pet1.Age = 2;
+
+			Console.WriteLine($"{pet1.Description}");
+			Pet pet2 = new Pet()
+			{
+				Name = "Пёска",
+				Kind = "Dog",
+				Sex = 'M',
+				Age = 4
+			};
+
+			Console.WriteLine($"{pet2.Description}");
+			Console.ReadKey();
+		}
+	}
+}
