0502bb097033a259fab5e1a7c9bd1a21da25d94c
L1ght <38190449+michaell1ght@users.noreply.github.com>
2019-04-22 17:42:32 +0300

    Update ILogWriter.cs

--- Lesson13/HomeWork/LogWriters/LogWriters/ILogWriter.cs
+++ Lesson13/HomeWork/LogWriters/LogWriters/ILogWriter.cs
@@ -6,6 +6,8 @@ namespace LogWriters
 {
     interface ILogWriter
     {
-        void LogInfo(string message);        void LogWarning(string message);        void LogError(string message);
+        void LogInfo(string message);
+        void LogError(string message);
+        void LogWarning(string message);
     }
 }
