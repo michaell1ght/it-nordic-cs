60fa51afa25bbe14bf3899559ee7ab36a7155909
L1ght <38190449+michaell1ght@users.noreply.github.com>
2019-06-04 10:58:41 +0300

    Create ReminderDatabase.sql

---
+++ Lesson27/Homework/ReminderDatabase.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE Reminder;
+GO
+
+USE Reminder;
+GO
+
+-- DROP TABLE dbo.ReminderItem
+CREATE TABLE dbo.ReminderItem (
+	Id INT NOT NULL,
+	DateToSend DATETIME NOT NULL,
+	ContactId INT NOT NULL,
+	Message VARCHAR(100) NOT NULL,
+	StatusId TINYINT NOT NULL,
+);
+
+GO
+ALTER TABLE dbo.ReminderItem
+ADD CONSTRAINT PK_ReminderItem PRIMARY KEY CLUSTERED (Id);
+GO
+
+-- DROP TABLE dbo.ReminderSendStatus
+CREATE TABLE dbo.ReminderSendStatus (
+	Id TINYINT NOT NULL,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+GO
+ALTER TABLE dbo.ReminderSendingStatus
+ADD CONSTRAINT PK_ReminderSendingStatus PRIMARY KEY CLUSTERED (Id);
+GO
+
+ALTER TABLE dbo.ReminderItem
+ADD CONSTRAINT FK_ReminderItem_StatusId FOREIGN KEY (StatusId)
+	REFERENCES dbo.ReminderSendStatus(Id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+GO
